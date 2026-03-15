bd417220525b9c99b1bbb9dc1958e772c51e7857
shivambhardwajwebdev <shiva.bhardwaj.mks@gmail.com>
2026-03-16 01:13:49 +0530

    global error handler

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,4 +2,8 @@ const express= require("express");
 const app = express();
 app.use(express.json())
 //4. Global error handler
+app.use((err,req,res,next)=>{
+    console.error(err.stack);
+    res.status(500).json({message:"Something went wrong on the server!"})
+})
 module.exports=app
